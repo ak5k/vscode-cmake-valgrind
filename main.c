7056724f258339c115cec087e27f72c0e1cf3cd4
ak5k <en5ca84@gmail.com>
2021-01-04 00:18:16 +0200

    dev

---
+++ main.c
@@ -0,0 +1,14 @@
+// #include <iostream>
+#include <stdio.h>
+
+#include <valgrind/memcheck.h>
+#include <valgrind/valgrind.h>
+
+int main(int argc, char const *argv[])
+{
+    /* code */
+    // VALGRIND_DO_LEAK_CHECK;
+
+    // std::cout << "Hello, world!\n";
+    return 0;
+}
